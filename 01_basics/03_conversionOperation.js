7c326681a137f89d62aafd578bdf56b097cd2806
Rutuja Vivek Kadam <127648693+rutuukadam54@users.noreply.github.com>
2024-11-09 13:51:28 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,35 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// **************Operation************
+let value =3
+let negvalue = -value
+console.log(negvalue)
+//console.log(2**3) ; 2 to power 3
+
+let str ="hello"
+let str1 = " Rutuja"
+let str3 = str + str1
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
