c949e3d5674cce1d0b8b79399db3a8b6920251f9
Rutuja Vivek Kadam <127648693+rutuukadam54@users.noreply.github.com>
2024-11-09 11:49:31 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14422
+let accountEmail = "Rutuja@gmail.com";
+var accountPassword = "552255"
+let accountState;
+
+accountCity = "Mumbai"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// Prefer not to use var. because of issue in block scope and functional scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+
+let name = " Rutuja" // string
+let age = 18
+let isLoggedIn = false
+
+//datatypes
+// number => 2 to power 53
+//bigint for big number
+//string => ""
+//boolean
+//null = standalone value
+//undefined =
+//symbol
+
+//object
+
+console.log(typeof "Rutuja");
+console.log(typeof null) // o/p : object
+console.log(typeof undefined) // o/p :undefined
+
